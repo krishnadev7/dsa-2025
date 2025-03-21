c7ae5f1f8cc223ebb9c3867ce4a51a24f241c783
krishnadev7 <krishnadevv12@gmail.com>
2025-03-21 20:08:19 +0530

    singly linkedlist

---
+++ src/Recursion/sortedArray.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class sortedArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,5,6,9};
+        System.out.println(sorted(arr,0));
+    }
+    static boolean sorted(int[] arr, int index){
+        if(index == arr.length - 1){
+            return true;
+        }
+        return arr[index] < arr[index + 1] && sorted(arr,index + 1);
+    }
+}

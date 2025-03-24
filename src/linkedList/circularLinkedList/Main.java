e61e4073e21bf4c149b519519c85ac710d2e0e0f
krishnadev7 <krishnadevv12@gmail.com>
2025-03-24 22:20:28 +0530

    circular linkedList

---
+++ src/linkedList/circularLinkedList/Main.java
@@ -0,0 +1,14 @@
+package linkedList.circularLinkedList;
+
+public class Main {
+    public  static  void main(String[] args){
+        CLL list = new CLL();
+        list.insert(10);
+        list.insert(12);
+        list.insert(13);
+        list.insert(14);
+        list.insert(15);
+        list.insert(16);
+        list.display();
+    }
+}
